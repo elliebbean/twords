b17dd48a4d06801d4d623842e2fb9f1635b855b1
avisgratis <avisgratis@gmail.com>
2023-06-15 23:13:25 +0100

    add version to the saves to ignore old saves

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -1,6 +1,7 @@
 import { GameMode, GameState } from "./game";
 
 const gameStatePrefix = "game-";
+const version = 0;
 
 export function loadGame(gameMode: GameMode): GameState | undefined {
   const gameStateJson = localStorage.getItem(gameStatePrefix + gameMode);
@@ -9,9 +10,17 @@ export function loadGame(gameMode: GameMode): GameState | undefined {
     return;
   }
 
-  return JSON.parse(gameStateJson) as GameState;
+  const gameState = JSON.parse(gameStateJson);
+
+  // at some point we should implement migration or something but it's fine for now to just ignore the old save
+  if ("_version" in gameState && gameState._version === version) {
+    return gameState as GameState;
+  } else {
+    return;
+  }
 }
 
 export function saveGame(gameState: GameState) {
-  localStorage.setItem(gameStatePrefix + gameState.settings.mode, JSON.stringify(gameState));
+  const stateToSave = { ...gameState, _version: version };
+  localStorage.setItem(gameStatePrefix + gameState.settings.mode, JSON.stringify(stateToSave));
 }
